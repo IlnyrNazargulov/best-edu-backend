7d4a5a59956353176946f3e7ce80a98df7fee364
IlnyrNazargulov <ilnnazargulov@gmail.com>
2021-03-25 12:51:01 +0300

    fix name for JsonProperty

--- web/src/main/java/ru/ilnyrdiplom/bestedu/web/contracts/requests/DisciplineRequest.java
+++ web/src/main/java/ru/ilnyrdiplom/bestedu/web/contracts/requests/DisciplineRequest.java
@@ -2,10 +2,12 @@ package ru.ilnyrdiplom.bestedu.web.contracts.requests;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 @Getter
 @Setter
 public class DisciplineRequest {
     private String name;
+    @JsonProperty("isPublic")
     private boolean isPublic;
 }
--- web/src/main/java/ru/ilnyrdiplom/bestedu/web/mixins/DisciplineMixin.java
+++ web/src/main/java/ru/ilnyrdiplom/bestedu/web/mixins/DisciplineMixin.java
@@ -1,8 +1,14 @@
 package ru.ilnyrdiplom.bestedu.web.mixins;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import ru.ilnyrdiplom.bestedu.facade.model.DisciplineFacade;
 
 @JsonSerialize(as = DisciplineFacade.class)
 public interface DisciplineMixin {
+    @JsonGetter("isPublic")
+    boolean isPublic();
+
+    @JsonGetter("isRemoved")
+    boolean isRemoved();
 }
